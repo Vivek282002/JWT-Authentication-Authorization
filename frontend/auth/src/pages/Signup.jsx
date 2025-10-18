1f08a0a33f83b93e825ef36cde166d4fe6044e0e
Vivek <Viveksde2002@gmail.com>
2025-10-18 10:14:55 +0530

    Updated Deployed Url to fetch api

--- frontend/auth/src/pages/Signup.jsx
+++ frontend/auth/src/pages/Signup.jsx
@@ -30,7 +30,7 @@ const Signup = () => {
             return handleError('name, email, password are required');
         }
         try{
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://jwt-authentication-authorization.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
